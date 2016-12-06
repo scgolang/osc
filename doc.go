0d30755c5b99f64f0f808f2853f3dbc9dd955fda
Brian Sorahan <bsorahan@gmail.com>
2016-12-06 14:02:12 -0600

    remove mention of hypebeast since the lib has pretty much been rewritten

--- doc.go
+++ doc.go
@@ -1,8 +1,6 @@
 /*
 Package osc implements Open Sound Control 1.0
 
-The code was forked from https://github.com/hypebeast/go-osc.
-
 For more info about OSC, go to http://opensoundcontrol.org
 */
 package osc
